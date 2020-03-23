669c02f2b940b07133c69044a6a39f0f9e78ee35
sugiuraryouga <61176802+sugiuraryouga@users.noreply.github.com>
2020-03-23 10:08:29 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get 'user/index'
   root "items#index"
   resources :users, only: [:index]
   resources :items, only: [:new,:show]
-  # root to: "user#index"
 end
