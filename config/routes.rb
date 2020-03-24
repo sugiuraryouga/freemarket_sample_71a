4407e242b04ef68f373b2435491c4c6033bc0f78
ryjtkd1204 <61186866+ryjtkd1204@users.noreply.github.com>
2020-03-24 16:44:52 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "items#index"
-  resources :users, only: [:index,:new]
+  root “items#index”
+  resources :users, only: [:index]
+  resources :items, only: [:new,:show]
 end
