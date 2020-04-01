df78f846706bbd021005dd11b9d56cd4f422468d
kento1117 <61189520+kento1117@users.noreply.github.com>
2020-04-01 17:41:44 +0900

    オーダーコントローラ

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,11 @@
+class OrdersController < ApplicationController
+  def show
+    @item=Item.find(params[:id])
+  end
+  # def create
+  #   @item = Item.new(item_params)
+  #   return if @item.valid?
+  #   render :new
+  # end
+  
+end
